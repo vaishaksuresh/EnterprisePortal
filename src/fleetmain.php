7de45ef9e7bcb73f020badc81d99af95ef0fa575
Vaishak Suresh <vaishak.suresh@gmail.com>
2011-08-04 22:00:29 +0530

    Image Changes

--- src/fleetmain.php
+++ src/fleetmain.php
@@ -46,20 +46,21 @@ $(function() {
 <div id="production" style="clear: both;" align="center">
 <table id="tbproduction" width="100%">
 	<tr>
-		<td><a id="fleetExcavator" href="#"><img align="middle" height="150px"
-			width="150px" src="./images/Excavator.jpg"></a></td>
-		<td><a id="fleetDumpers" href="#"><img height="150px" width="150px"
-			src="./images/Dumpers.jpg"></a></td>
-		<td><a id="fleetCompressors" href="#"><img height="150px"
-			width="150px" src="./images/Compressors.jpg"></a></td>
+		<td><center><a id="fleetExcavator" href="#"><img align="middle" height="150px"
+			width="150px" src="./images/Excavator.jpg"></a>
+			<br><b>Excavators</b></center></td>
+		<td><center><a id="fleetDumpers" href="#"><img height="150px" width="150px"
+			src="./images/Dumpers.jpg"></a><br><b>Dumpers</b></center></td>
+		<td><center><a id="fleetCompressors" href="#"><img height="150px"
+			width="150px" src="./images/Compressors.jpg"></a><br><b>Compressors</b></center></td>
 	</tr>
 	<tr>
-		<td><a id="fleetGenerators" href="#"><img height="150px" width="150px"
-			src="./images/Generators.jpg"></a></td>
-		<td><a id="fleetVehicles" href="#"><img height="150px" width="150px"
-			src="./images/Vehicles.gif"></a></td>
-		<td><a id="fleetWiresaw" href="#"><img height="150px" width="150px"
-			src="./images/Wire saw.jpg"></a></td>
+		<td><center><a id="fleetGenerators" href="#"><img height="150px" width="150px"
+			src="./images/Generators.jpg"></a><br><b>Generators</b></center></td>
+		<td><center><a id="fleetVehicles" href="#"><img height="150px" width="150px"
+			src="./images/Vehicles.gif"></a><br><b>Other Vehicles</b></center></td>
+		<td><center><a id="fleetWiresaw" href="#"><img height="150px" width="150px"
+			src="./images/Wire saw.jpg"></a><br><b>Wire Saw</b></center></td>
 	</tr>
 </table>
 </div>
